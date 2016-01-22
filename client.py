9b32220eeac883abfdafb5e795f6c53aae578316
Nathan Miller <nathan@sellerengine.com>
2016-01-21 23:29:06 -0800

    Environment consistency & __main__

--- client.py
+++ client.py
@@ -6,17 +6,34 @@ from u import cachedProperty
 class TrelloClient(object):
     ENDPOINT = 'https://trello.com/1/'
     NAME = 'Dingo Thaw'
+    KEY_NAME = 'TRELLO_KEY'
+    TOKEN_NAME = 'TRELLO_TOKEN'
+
     @property
     def key(self):
-        return os.environ.get('TRELLO_KEY')
+        return os.environ[self.KEY_NAME]
+
+    @key.setter
+    def key(self, key):
+        os.environ[self.KEY_NAME] = key
 
     @property
     def token(self):
-        return os.environ.get('TRELLO_TOKEN')
+        if self.TOKEN_NAME not in os.environ:
+            request = requests.Request('GET', self.url('authorize'),
+                    params=self.token_params).prepare()
+            print 'No token set in ${}.'.format(self.TOKEN_NAME)
+            print 'Go to {.url} to generate one and paste it here: '.format(request)
+            self.token = raw_input()
+        return os.environ[self.TOKEN_NAME]
 
     @token.setter
     def token(self, token):
-        os.environ['TRELLO_TOKEN'] = token
+        os.environ[self.TOKEN_NAME] = token
+
+    @property
+    def params(self):
+        return dict(key=self.key, token=self.token)
 
     @property
     def token_params(self):
@@ -30,29 +47,19 @@ class TrelloClient(object):
     def url(self, *components):
         return urlparse.urljoin(self.ENDPOINT, '/'.join(components))
 
-    def _ensure_token(self):
-        if self.token:
-            return
-
-        request = requests.Request('GET', self.url('authorize'),
-                params=self.token_params).prepare()
-        print 'No token set in $TRELLO_TOKEN.'
-        print 'Go to {.url} to generate one and paste it here: '.format(request)
-        self.token = raw_input()
-
-    def _request(self, method, components, data=None, params=None):
-        self._ensure_token()
-        url = self.url(*components)
-        params = dict(params or {}, key=self.key, token=self.token)
-        response = self.session.request(method=method, url=url, data=data, params=params)
+    def _request(self, method, components, data=None, params={}):
+        response = self.session.request(method=method,
+                                        url=self.url(*components),
+                                        data=data,
+                                        params=dict(self.params, **params))
         response.raise_for_status()
         return response.json()
 
     def get(self, components, params=None):
         return self._request('GET', components, params=params)
 
-    def post(self, components, params=None):
-        return self._request('POST', components, data=params)
+    def post(self, components, data=None):
+        return self._request('POST', components, data=data)
 
     def put(self, components, data=None):
         return self._request('PUT', components, data=data)
@@ -61,6 +68,6 @@ class TrelloClient(object):
         return self._request('DELETE', components)
 
 
-
-# 'https://trello.com/1/authorize?key=%s&name=%s&expiration=%s&response_type=token&scope=%s'
-# % (self._apikey, quote_plus(app_name), expires, 'read,write' if write_access else 'read')
+if __name__ == '__main__':
+    client = TrelloClient()
+    import IPython;IPython.embed()
